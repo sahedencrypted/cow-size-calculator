4cef165b05303a796286660d54737a0dc4da6f23
darkcat-sudo <130399755+darkcat-sudo@users.noreply.github.com>
2024-06-18 16:07:19 +0600

    add 2nd round

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const unitSelect = document.getElementById('unit');
   const girthLabel = document.getElementById('one-top');
   const lengthLabel = document.getElementById('two-top');
-  const McostResult = document.getElementById('m-result')
+  const McostResult = document.getElementById('m-result');
+  const Mperch = document.getElementById('m-perch');
+  const costMeat = document.getElementById('cost-meat');
 
+
+let weight;
+let cost;
 let unit ='inches';
   unitSelect.addEventListener('change', () => {
     unit = unitSelect.value;
     girthLabel.textContent = `Heart Girth (${unit})`;
     lengthLabel.textContent = `Body Length (${unit})`;
-    resultCal()
   });
-  function resultCal(){
-    
+  Mperch.addEventListener('change', () => {
+    meatkg();
+  });
+  function meatkg(){
+    const meatweight = Math.floor(weight*(Mperch.value/100));
+    McostResult.textContent = `${meatweight} kg  ~  ${(meatweight/40).toFixed(2)} মণ`;
+    const meattk = Math.floor(cost*(90/100));
+    costMeat.textContent = `${Math.floor(meattk/meatweight)} TK per kg`;
   }
-let weight;
+
+
   form.addEventListener('input', () => {
     const formData = new FormData(form);
     const values = Object.fromEntries(formData.entries());
@@ -33,7 +44,8 @@ let weight;
       else if(unit == 'inches'){
         weight = (girth * girth * length) / 660;
         weightResult.textContent = `${weight.toFixed(2)} kg`;
-        McostResult.textContent = `${Math.floor(weight*0.48)} kg`;
+        meatkg();
+        
       }
       else if(unit == 'Foot'){
         weight = ((girth*12) * (girth*12) * (length*12)) / 660;
@@ -43,8 +55,9 @@ let weight;
       }
 
       if (pricePerKg > 0) {
-        const cost = weight * pricePerKg;
-        costResult.textContent = `${Math.floor(cost)} Tk`;
+        cost = Math.floor(weight * pricePerKg);
+        costResult.textContent = `${cost} Tk`;
+        meatkg();
       } else {
         costResult.textContent = '';
       }
